e2dae41a61b9df5ca4b15734a1c5e25c2ba2db19
1594510003@qq.com <1594510003@qq.com>
2022-02-21 04:50:59 -0800

    first commit

---
+++ src/HelloWorld_assets/src/index.js
@@ -0,0 +1,19 @@
+import { HelloWorld } from "../../declarations/HelloWorld";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await HelloWorld.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
